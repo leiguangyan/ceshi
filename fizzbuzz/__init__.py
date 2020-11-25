458654f83e695cc80bc16ca20c22bae43b104cbe
leiguangyan <lei1402@qq.com>
2020-11-25 21:02:25 +0800

    第一次
    第一次

---
+++ fizzbuzz/__init__.py
@@ -0,0 +1,27 @@
+""" Fizzbuzz """
+
+def process_file(path):
+    """process the test data"""
+    result = []
+    with open(path, "r") as file:
+        line = file.readline()
+        while line:
+            number = int(line)
+            result.append(str(fizzbuzz(number)))
+            line = file.readline()
+    return "\n".join(result)
+
+def __is_divide_by_or_contains(number, divisor):
+    return number % divisor == 0 \
+        or str(number).find(str(divisor)) != -1
+
+def fizzbuzz(number):
+    """fizzbuzz"""
+    result = ""
+    if __is_divide_by_or_contains(number, 3):
+        result += "Fizz"
+    if __is_divide_by_or_contains(number, 5):
+        result += "Buzz"
+    if result != "":
+        return result
+    return number
---
+++ tests/test_fizzbuzz.py
@@ -0,0 +1,40 @@
+import fizzbuzz
+
+def test_1_got_1():
+    assert fizzbuzz.fizzbuzz(1) == 1
+
+def test_2_return_2():
+    assert fizzbuzz.fizzbuzz(2) == 2
+
+def test_3_return_fizz():
+    assert fizzbuzz.fizzbuzz(3) == "Fizz"
+
+def test_5_return_buzz():
+    assert fizzbuzz.fizzbuzz(5) == "Buzz"
+
+def test_13_return_buzz():
+    assert fizzbuzz.fizzbuzz(13) == "Fizz"
+
+def test_15_retrun_fizzbuzz():
+    assert fizzbuzz.fizzbuzz(15) == "FizzBuzz"
+
+def test_51_return_fizzbuzz():
+    assert fizzbuzz.fizzbuzz(51) == "FizzBuzz"
+
+def test_process_all():
+    assert fizzbuzz.process_file("./resource/testData.txt") == """1
+2
+Fizz
+4
+Buzz
+Fizz
+7
+8
+Fizz
+Buzz
+11
+Fizz
+Fizz
+14
+FizzBuzz
+16"""
